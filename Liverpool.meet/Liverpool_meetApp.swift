760e1b7492c9231d39eb5d8fc73926aa490e88fc
jose julian Lopez Huacuja <jose.lopez4636@alumnos.udg.mx>
2025-05-13 10:50:32 -0600

    Initial Commit

---
+++ Liverpool.meet/Liverpool_meetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liverpool_meetApp.swift
+//  Liverpool.meet
+//
+//  Created by Jose julian Lopez on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Liverpool_meetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
